80e154902a6454f403a4776d6584b671270f7809
Shadab Khan <iam.shadab.khan.1001@gmail.com>
2025-02-22 22:13:44 +0530

    third production build error resolve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-// Fixing ESLint issue by explicitly defining a parser
-const eslintConfig = [
+export default [
+  ...compat.extends("next/core-web-vitals"),
   {
-    parser: "@babel/eslint-parser",
-    extends: ["next/core-web-vitals"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
   },
 ];
-
-export default eslintConfig;
